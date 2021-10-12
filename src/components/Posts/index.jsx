901ca51c769219d490024f0725dfcf9a61709472
Vinícius Lucena <jviniiciuslucena@gmail.com>
2021-10-12 19:10:27 -0300

    Commit referente até a aula 22 do curso

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => {
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          title={post.title}
+          body={post.body}
+          cover={post.cover}
+          id={post.id}
+        />
+      ))}
+    </div>
+  );
+};
